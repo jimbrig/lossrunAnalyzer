d956c2dd465df9dae62e4ea4d57542a4e8ae12a8
Jimmy Briggs <jimmy.briggs@oliverwyman.com>
2020-01-10 15:01:08 -0500

    description Title

--- devhist.R
+++ devhist.R
@@ -17,26 +17,24 @@ library(desc)
 # ignore this script
 usethis::use_build_ignore("devhist.R")
 
+# add description to DESCRIPTION
+desc::desc_set("Title" = "R Package and Shiny App to Analyze Insurance Lossruns")
+
 # setup git / version control
 usethis::edit_r_profile() # set ssh as default protocol
-
 my_cred <- git2r::cred_ssh_key(
   publickey = fs::path_abs('C:\\Users\\jimmy.briggs\\.ssh\\id_rsa.pub'),
   privatekey = fs::path_abs('C:\\Users\\jimmy.briggs\\.ssh\\id_rsa')
 )
-
 usethis::use_git_credentials(my_cred)
 usethis::git_credentials()
 rm(my_cred)
-
 usethis::use_git_config(user.name = "Jimmy Briggs", user.email = "jimmy.briggs@oliverwyman.com")
-
 usethis::use_git()
 usethis::git_vaccinate()
-usethis::use_git_remote()
-
+usethis::use_github()
 usethis::git_sitrep()
 
 ?usethis::use_github()
 
-usethis::use_github()
+
